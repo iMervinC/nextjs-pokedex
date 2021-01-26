feaaafdf2e8c5495ac70a64225ae788df8798b9b
Mervin Christian T. Itorma <40752994+iMervinC@users.noreply.github.com>
2021-01-26 14:26:31 +0800

    d

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  purge: [],
+  purge: { layers: ['components', 'utilities'], content: ['./src/**/*.html'] },
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
